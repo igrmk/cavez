52da2ffd5395cae12d0fdccca002d7c944bb0a54
Igor Mikushkin <igor.mikushkin@gmail.com>
2023-09-15 21:12:00 +0100

    Don't activate environment if it is already activated

--- cavez.plugin.zsh
+++ cavez.plugin.zsh
@@ -1,25 +1,26 @@
 _cavez_find_up() {
-	[[ $2 ]] || { _cavez_find_up "$1" "$PWD"; return; }
 	[[ / == "$2" ]] && return 1 || [[ -e "$2/$1" ]] && echo "$2/$1" && return || _cavez_find_up "$1" "$(dirname "$2")"
 }
 
-alias _cavez_conda_implementation=conda
-
-if ! type conda > /dev/null; then
-	if type mamba > /dev/null; then
-		alias _cavez_conda_implementation=mamba
-	elif type micromamba > /dev/null; then
-		alias _cavez_conda_implementation=micromamba
-	fi
+if type conda > /dev/null; then
+	alias _cavez_conda_implementation=conda
+elif type mamba > /dev/null; then
+	alias _cavez_conda_implementation=mamba
+elif type micromamba > /dev/null; then
+	alias _cavez_conda_implementation=micromamba
+else
+	alias _cavez_conda_implementation=conda
 fi
 
 export _CAVEZ_AUTO_ACTIVATED=0
 
 _cavez_auto_activate() {
-	found="$(_cavez_find_up "${CAVEZ_VENV_DIR_NAME:-.venv}")"
+	found="$(_cavez_find_up "${CAVEZ_VENV_DIR_NAME:-.venv}" "$PWD")"
 	if [[ $? -eq 0 ]]; then
-		_cavez_conda_implementation activate "$found"
-		export _CAVEZ_AUTO_ACTIVATED=1
+		if [[ "$_CAVEZ_AUTO_ACTIVATED" == 0 ]]; then
+			_cavez_conda_implementation activate "$found"
+			export _CAVEZ_AUTO_ACTIVATED=1
+		fi
 	elif [[ "$_CAVEZ_AUTO_ACTIVATED" == 1 ]]; then
 		_cavez_conda_implementation deactivate
 		export _CAVEZ_AUTO_ACTIVATED=0
