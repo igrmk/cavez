67bc83e2b0e814ab2e91a58dc5d3cd6b61ab6650
Igor Mikushkin <igor.mikushkin@gmail.com>
2023-09-16 14:38:10 +0100

    Cosmetic style changes

--- cavez.plugin.zsh
+++ cavez.plugin.zsh
@@ -1,31 +1,31 @@
-_cavez_find_up() {
-	[[ / == "$2" ]] && return 1 || [[ -e "$2/$1" ]] && echo "$2/$1" && return || _cavez_find_up "$1" "$(dirname "$2")"
+_cavez::find_up() {
+	[[ / == "$2" ]] && return 1 || [[ -e "$2/$1" ]] && echo "$2/$1" && return || _cavez::find_up "$1" "$(dirname "$2")"
 }
 
 if type conda > /dev/null; then
-	alias _cavez_conda_implementation=conda
+	alias _cavez::conda_implementation=conda
 elif type mamba > /dev/null; then
-	alias _cavez_conda_implementation=mamba
+	alias _cavez::conda_implementation=mamba
 elif type micromamba > /dev/null; then
-	alias _cavez_conda_implementation=micromamba
+	alias _cavez::conda_implementation=micromamba
 else
-	alias _cavez_conda_implementation=conda
+	alias _cavez::conda_implementation=conda
 fi
 
-_cavez_auto_activate() {
-	local found="$(_cavez_find_up "${CAVEZ_VENV_DIR_NAME:-.venv}" "$PWD")"
+_cavez::auto_activate() {
+	local found="$(_cavez::find_up "${CAVEZ_VENV_DIR_NAME:-.venv}" "$PWD")"
 	[[ -n "$found" ]] && local found="$(realpath "$found")"
 
 	if [[ -n "$_CAVEZ_AUTO_ACTIVATED" ]] && [[ "$_CAVEZ_AUTO_ACTIVATED" != "$found" ]]; then
-		_cavez_conda_implementation deactivate
+		_cavez::conda_implementation deactivate
 		unset _CAVEZ_AUTO_ACTIVATED
 	fi
 
 	if [[ -n "$found" ]] && [[ "$_CAVEZ_AUTO_ACTIVATED" != "$found" ]]; then
-		_cavez_conda_implementation activate "$found"
+		_cavez::conda_implementation activate "$found"
 		export _CAVEZ_AUTO_ACTIVATED="$CONDA_PREFIX"
 	fi
 }
 
-_cavez_auto_activate
-chpwd_functions+=(_cavez_auto_activate)
+_cavez::auto_activate
+chpwd_functions+=(_cavez::auto_activate)
