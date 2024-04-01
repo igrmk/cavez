040daf92ca649d284baa3d05f724242e4821a4be
Igor Mikushkin <igor.mikushkin@gmail.com>
2024-04-01 02:30:11 +0100

    Fix aliases, part 2

--- cavez.plugin.zsh
+++ cavez.plugin.zsh
@@ -50,4 +50,4 @@ _cavez::auto_activate_conda_env
 chpwd_functions+=(_cavez::auto_activate_conda_env)
 
 alias cavez-create-new-env='_cavez::conda_flavour create -p ./"${CAVEZ_VENV_DIR_NAME:-.venv}"'
-alias cavez-activate='_cavez::conda_flavour activate ./.venv'
+alias cavez-activate='_cavez::conda_flavour activate ./"${CAVEZ_VENV_DIR_NAME:-.venv}"'
